30784400ba834a824f9fa8737b45dd98c6b993d6
Xinran Zhu <xz584@cornell.edu>
2020-06-25 10:54:27 -0700

    update hypre results

--- examples/hypre_postprocess/hypre_parse_plot.sh
+++ examples/hypre_postprocess/hypre_parse_plot.sh
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 set -x 
 
 nmax=200
@@ -5,10 +6,20 @@ nmin=10
 ntask=60
 equation="convdiff"
 # equation="Poisson"
+=======
+nmax=200
+nmin=100
+ntask=30
+equation="convdiff"
+>>>>>>> update hypre results
 nrun=10
 
 # read results
 python parse_tuning_results.py --nmax ${nmax} --nmin ${nmin} --ntask ${ntask} --equation ${equation} 
 # plot
+<<<<<<< HEAD
 python hypre_plot.py --nmax ${nmax} --nmin ${nmin} --ntask ${ntask} --equation ${equation} --nrun ${nrun}
 
+=======
+python hypre_plot.py --nmax ${nmax} --nmin ${nmin} --ntask ${ntask} --equation ${equation} --nrun ${nrun}
+>>>>>>> update hypre results
--- examples/hypre_postprocess/hypre_plot.py
+++ examples/hypre_postprocess/hypre_plot.py
@@ -5,8 +5,11 @@ import pickle
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+<<<<<<< HEAD
 from sklearn.linear_model import LinearRegression
 
+=======
+>>>>>>> update hypre results
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -27,16 +30,21 @@ def data_process(args):
     ntask = len(results_summary)
     assert ntask == args.ntask
     OpenTunervsGPTune = []
+<<<<<<< HEAD
     HpBandstervsGPTune = []
     GPTune_time = []
     Opentuner_time = []
     HpBandster_time = []
     size_set = []
+=======
+    HpBanstervsGPTune = []
+>>>>>>> update hypre results
     for i in range(ntask):
         task_current = results_summary[i]
         idx_nrun = task_current[2].index(args.nrun)
         results_GPTune = task_current[3][0]
         results_OpenTuner = task_current[3][1]
+<<<<<<< HEAD
         results_HpBandster = task_current[3][2]
         assert results_GPTune[0] == "GPTune"
         assert results_OpenTuner[0] == "OpenTuner"
@@ -48,6 +56,16 @@ def data_process(args):
         HpBandster_time.append(results_HpBandster[1][idx_nrun])
         size_set.append(task_current[4])
     return OpenTunervsGPTune, HpBandstervsGPTune, GPTune_time, Opentuner_time, HpBandster_time, size_set
+=======
+        results_Hpbanster = task_current[3][2]
+        assert results_GPTune[0] == "GPTune"
+        assert results_OpenTuner[0] == "OpenTuner"
+        assert results_Hpbanster[0] == "Hpbandster"
+        OpenTunervsGPTune.append(results_OpenTuner[1][idx_nrun]/results_GPTune[1][idx_nrun])
+        HpBanstervsGPTune.append(results_Hpbanster[1][idx_nrun]/results_GPTune[1][idx_nrun])
+    
+    return OpenTunervsGPTune, HpBanstervsGPTune
+>>>>>>> update hypre results
 
 def plot(data1, data2, args):
     my_source = gen_source(args)
@@ -57,6 +75,7 @@ def plot(data1, data2, args):
     
     p1 = len([x for x in data1 if x >= 1])
     p2 = len([x for x in data2 if x >= 1])
+<<<<<<< HEAD
     p3 =  len([x for x in data1 if x < 0.5])
     p4 =  len([x for x in data2 if x < 0.5])
     
@@ -153,5 +172,30 @@ def main(args):
     # plot_histogram(OpenTunervsGPTune, HpBandstervsGPTune, args)
     # plot_size_time(GPTune_time, Opentuner_time, HpBandster_time, size_set, args)
     
+=======
+
+    # plot
+    x = np.arange(1, ntask+1)
+    width = 0.35  # the width of the bars
+    fig, ax = plt.subplots()
+    ax.bar(x - width/2, data1, width, label=f'OpenTuner/GPTune, {p1}')
+    ax.bar(x + width/2, data2, width, label=f'HpBanster/GPTune, {p2}')
+    ax.plot([0,ntask+1], [1, 1], c='black')
+    ax.set_ylabel('ratio of best performance')
+    ax.set_title(f'{args.equation}, [nx, ny, nz] in [{args.nmin}, {args.nmax}], nrun = {nrun}')
+    ax.legend()
+    fig.tight_layout()
+    
+    
+    filename = os.path.splitext(os.path.basename(my_source))[0]
+    fig.savefig(os.path.join("./plots", f"{filename}_nrun{nrun}.pdf"))
+
+def main(args):
+    OpenTunervsGPTune, HpBanstervsGPTune = data_process(args)
+    print(OpenTunervsGPTune)
+    print(HpBanstervsGPTune)
+    plot(OpenTunervsGPTune, HpBanstervsGPTune, args)
+
+>>>>>>> update hypre results
 if __name__ == "__main__":
     main(parse_args())
