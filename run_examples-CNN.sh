cb57ffa9f4749a56527ba3ede2ba06104e5a0637
Xinran Zhu <xz584@cornell.edu>
2021-04-13 19:06:11 -0700

    minor fix

--- run_examples-CNN.sh
+++ run_examples-CNN.sh
@@ -336,7 +336,7 @@ if [[ $ModuleEnv == *"openmpi"* ]]; then
         app_json=$(echo "{\"tuning_problem_name\":\"$tp\"")
         echo "$app_json$machine_json$software_json$loadable_machine_json$loadable_software_json}" | jq '.' > .gptune/meta.json 
         $MPIRUN --oversubscribe --allow-run-as-root --mca pmix_server_max_wait 3600 --mca pmix_base_exchange_timeout 3600 --mca orte_abort_timeout 3600 --mca plm_rsh_no_tree_spawn true -n 1  \
-        python ./strumpack_MLA_KRR.py -ntask 1 -nrun 10 -machine cori -npernode $cores -optimization "GPTune"\
+        python ./cnnMNIST_MB.py -ntask 1 -nrun 10 -machine cori -npernode $cores -optimization "GPTune"\
         2>&1 | tee a.out_CNNMNIST_ntask1_nrun10_GPTune
         # multi-fidelity run
         
